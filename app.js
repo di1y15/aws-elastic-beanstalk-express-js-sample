322d7e65caaa059760485625178cdd229b6e6779
di1y15 <ilyasovkz@gmail>
2022-11-06 16:25:50 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Daulet, your dreams will come true soon!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
